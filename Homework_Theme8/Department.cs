51764264ed8ff82487633e7e13cb53e883168bc0
Сергей Воробьев <kazan1003@gmail.com>
2021-10-14 17:02:57 +0300

    worker and department created

---
+++ Homework_Theme8/Department.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Theme8
+{
+    struct Department
+    {
+
+
+        #region Поля
+
+        /// <summary>
+        /// номер Департамента
+        /// </summary>
+        int depId;
+
+        /// <summary>
+        /// Название
+        /// </summary>
+        private string depName;
+
+        /// <summary>
+        /// Дата создания
+        /// </summary>
+        private DateTime creationDate;
+
+        ///// <summary>
+        ///// Количество сотрудников в департаменте
+        ///// </summary>
+        //private uint workersCount;
+
+        /// <summary>
+        /// Массив с работниками
+        /// </summary>
+        public List<Worker> workers;
+
+        /// <summary>
+        /// Заголовки
+        /// </summary>
+        string[] titles;
+
+        #endregion
+
+        #region Свойства
+
+        ///// <summary>
+        ///// Индекс
+        ///// </summary>
+        //public int Index { get { return this.index; } set { this.index = value; } }
+
+        /// <summary>
+        /// Название
+        /// </summary>
+        public string DepName { get { return this.depName; } set { this.depName = value; } }
+
+        /// <summary>
+        /// Дата создания
+        /// </summary>
+        public DateTime CreaationDate { get { return this.creationDate; } set { this.creationDate = value; } }
+
+        ///// <summary>
+        ///// Количество работников
+        ///// </summary>
+        //public int WorkersCount { get { return this.index; } }
+
+        #endregion
+
+        #region Конструктор
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="depNumber">номер департамента</param>
+        /// <param name="empCount">количество работников</param>
+        public Department(int depNumber,int empCount)
+        {
+            Random r = new Random();
+            
+            this.depId = depNumber;        
+            this.depName = $"Department № {depNumber}";
+            this.creationDate = new DateTime(2020, 10, depNumber);
+            this.titles = new string[7] {"id","Имя", "Фамилия", "Возраст", "Департамент", "Зарплата", "Проектов", };
+            this.workers = new List<Worker>();
+
+            for (int i = 1; i <= empCount; i++)
+            {
+                workers.Add(
+                    new Worker(
+                        (uint)(depNumber*1000+i),
+                        $"Имя_{i}",
+                        $"Фамилия_{i}",
+                        (byte)r.Next(20,100),
+                        (uint)r.Next(1000, 2000),
+                        this.depName,
+                        (byte)r.Next(1,5)));
+            }
+
+        }
+
+        #endregion
+
+        #region Методы
+
+        /// <summary>
+        /// Печать в консоль
+        /// </summary>
+        public void PrintDepToConsole()
+        {
+            Console.WriteLine($"\nДепартамент № {depId}");
+            Console.WriteLine($"{titles[0],3} {titles[1],10} {titles[2],20} {titles[3],10} {titles[4],15}  {titles[5],15} {titles[6],10}" );
+            foreach (var item in workers)
+            {
+                Console.WriteLine(item.Print());
+            }
+        }
+
+        public void OrderDepartment()
+        {
+            var sortedTmp = from r in this.workers 
+                            orderby r.Age
+                            select r;
+
+            this.workers = sortedTmp.ToList(); ;
+        }
+
+        #endregion
+    }
+}
---
+++ Homework_Theme8/Worker.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Theme8
+{
+    struct Worker
+    {
+        #region Конструкторы
+
+        /// <summary>
+        /// Создание сотрудника
+        /// </summary>
+        /// <param name="FirstName">Имя</param>
+        /// <param name="LastName">Фамилия</param>
+        /// <param name="Position">Должность</param>
+        /// <param name="Department">Отдел</param>
+        /// <param name="Salary">Оплата труда</param>
+        public Worker(uint ID, string FirstName, string LastName, byte Age, uint Salary, string Department,byte ProjectsCount)
+        {
+            this.id = ID;
+            this.firstName = FirstName;
+            this.lastName = LastName;
+            this.age = Age;
+            this.department = Department;
+            this.salary = Salary;
+            this.projectsCount = ProjectsCount;
+        }
+
+        #endregion
+
+        #region Методы
+
+        public string Print()
+        {
+            return $"{this.id,5} {this.firstName,10} {this.lastName,20} {this.age,5} {this.department,20}  {this.salary,10} {this.projectsCount,8}";
+        }
+
+        #endregion
+
+        #region Свойства
+
+        public uint Id { get { return this.id; } }
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string FirstName { get { return this.firstName; } set { this.firstName = value; } }
+
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        public string LastName { get { return this.lastName; } set { this.lastName = value; } }
+
+        /// <summary>
+        /// Возраст
+        /// </summary>
+        public byte Age { get { return this.age; } set { this.age = value; } }
+
+        /// <summary>
+        /// Отдел
+        /// </summary>
+        public string Department { get { return this.department; } set { this.department = value; } }
+
+        /// <summary>
+        /// Оплата труда
+        /// </summary>
+        public uint Salary { get { return this.salary; } set { this.salary = value; } }
+
+        /// <summary>
+        /// Количество проектов
+        /// </summary>
+        public byte ProjectsCount { get { return this.projectsCount; } set { this.projectsCount = value; } }
+
+        /// <summary>
+        /// Почасовая оплата
+        /// </summary>
+        public double HourRate
+        {
+            get
+            {
+                byte workingDays = 25; // Рабочих дней в месяце
+                byte workingHours = 8; // Рабочих часов в день
+                return ((double)Salary) / workingDays / workingHours;
+            }
+        }
+
+        #endregion
+
+        #region Поля
+
+        /// <summary>
+        /// Уникальный номер
+        /// </summary>
+        private uint id;
+
+        /// <summary>
+        /// Поле "Имя"
+        /// </summary>
+        private string firstName;
+
+        /// <summary>
+        /// Поле "Фамилия"
+        /// </summary>
+        private string lastName;
+
+        /// <summary>
+        /// Возраст
+        /// </summary>
+        private byte age;
+
+        /// <summary>
+        /// Поле "Отдел"
+        /// </summary>
+        private string department;
+
+        /// <summary>
+        /// Поле "Оплата труда"
+        /// </summary>
+        private uint salary;
+
+        /// <summary>
+        /// Количество закрепленных проектов
+        /// </summary>
+        private byte projectsCount;
+
+        #endregion
+    }
+}
